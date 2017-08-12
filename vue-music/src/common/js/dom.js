c4a89880cbd63a243f8c0cb5108534128bd8d166
Aliennnnnn <329773905@qq.com>
2017-08-12 23:23:14 +0800

    music-list组件完工
    歌手详情页面组件完成，增加了下拉图片放大、高斯模糊等

--- vue-music/src/common/js/dom.js
+++ vue-music/src/common/js/dom.js
@@ -21,3 +21,34 @@ export function getData(el, name, val){
 	}
 	return el.getAttribute(prefix + name)
 }
+
+//利用浏览器能力检测，为CSS自动添加前缀
+let elementStyle = document.createElement('div').style
+let vendor = (() => {
+	let tranformNames = {
+		webkit: 'webkitTransform',
+		Moz: 'MozTransform',
+		O: 'oTransform',
+		ms: 'msTransform',
+		standard: 'transform'
+	}
+
+	for(let key in tranformNames){
+		if(elementStyle[tranformNames[key]] !== undefined){
+			return key
+		}
+	}
+	return false
+})()
+
+export function prefixStyle(style){
+	if(vendor === false){
+		return false
+	}
+
+	if(vendor === 'standard'){
+		return style
+	}
+
+	return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
--- vue-music/src/components/music-list/music-list.vue
+++ vue-music/src/components/music-list/music-list.vue
@@ -1,13 +1,19 @@
 <template>
 	<div class="music-list">
-		<div class="back">
+		<div class="back" @click="back">
 			<i class="icon-back">
 				
 			</i>
 		</div>
 		<h1 class="title" v-html="title"></h1>
 		<div class="bg-image" :style="bgStyle" ref="bgImage">
-			<div class="filter"></div>
+      <div class="play-wrapper">
+        <div class="play" v-show="songs.length>0" ref=playBtn>
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+			<div class="filter" ref="filter"></div>
 		</div>
 		<div class="bg-layer" ref="layer">
 			
@@ -15,6 +21,9 @@
 		<scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" :data="songs" class="list" ref="list">
 			<div class="song-list-wrapper">
 				<song-list :songs="songs"></song-list>
+        <div class="loading-container" v-show="!songs.length">
+          <loading></loading>
+        </div>
 			</div>
 		</scroll>
 	</div>
@@ -23,8 +32,12 @@
 <script type="text/javascript">
 	import songList from '../../base/song-list/song-list.vue'
 	import Scroll from '../../base/scroll/scroll.vue'
+  import Loading from '../../base/loading/loading.vue'
+  import {prefixStyle} from '../../common/js/dom.js'
 
 	const RESERVER_HEIGHT = 40
+  const transform = prefixStyle('transform')
+  const backdrop = prefixStyle('backdrop-filter')
 
 	export default {
 		created() {
@@ -62,30 +75,52 @@
 		},
 		components: {
 			Scroll,
-			songList
+			songList,
+      Loading
 		},
 		methods: {
 			scroll(pos){
 				this.scrollY = pos.y
-			}
+			},
+      back(){
+        this.$router.back()
+      }
 		},
 		watch: {
 			scrollY(newY){
 				let translateY = Math.max(this.minTranslateY,newY)
 				let zIndex = 0
-				this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
-				this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)`
+        //图片放大倍数
+        let scale = 1
+        //下拉高斯模糊
+        let blur = 0
+        const percent = Math.abs(newY / this.imageHeight)
 
-				if(newY < this.minTranslateY){
-					zIndex = 10
-					this.$refs.bgImage.style.paddingTop = 0
-					this.$refs.bgImage.style.height = `${RESERVER_HEIGHT}px`
-				}else{
-					this.$refs.bgImage.style.paddingTop = '70%'
-					this.$refs.bgImage.style.height = 0
-				}
-				this.$refs.bgImage.style.zIndex = zIndex
-			}
+        if(newY > 0){
+          scale = 1 + percent
+          zIndex = 10
+        }else{
+          blur = Math.min(20*percent,20)
+        }
+        //IOS图片下拉高斯模糊，Android不支持
+        this.$refs.filter.style['backdrop-filter'] = `blur(${blur}px)`
+
+        //layer
+    		this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
+
+    		if(newY < this.minTranslateY){
+    			zIndex = 10
+    			this.$refs.bgImage.style.paddingTop = 0
+    			this.$refs.bgImage.style.height = `${RESERVER_HEIGHT}px`
+                this.$refs.playBtn.style.display = 'none'
+    		}else{
+    			this.$refs.bgImage.style.paddingTop = '70%'
+    			this.$refs.bgImage.style.height = 0
+                this.$refs.playBtn.style.display = ''
+    		}
+    		this.$refs.bgImage.style.zIndex = zIndex
+        this.$refs.bgImage.style['transform'] = `scale(${scale})`
+	    }
 		}
 	}
 </script>
