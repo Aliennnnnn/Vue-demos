8e971175b8924060063276678e8282cb55fa00fa
Aliennnnnn <329773905@qq.com>
2017-08-26 11:19:43 +0800

    config

--- vue-music/build/webpack.base.conf.js
+++ vue-music/build/webpack.base.conf.js
@@ -22,6 +22,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
+      //'vue$': 'vue/dibust/vue.esm.js',
       '@': resolve('src'),
       'common': resolve('src/common'),
       'components': resolve('src/components')
