13617059610adcfd4b0f1668371422462a8b6fd4
Aliennnnnn <329773905@qq.com>
2017-08-29 18:40:39 +0800

    跨域

--- vue-music/src/api/recommend.js
+++ vue-music/src/api/recommend.js
@@ -14,26 +14,8 @@ export function getRecommend(){
 	return jsonp(url, data, options)
 }
 
-//歌单数据
-// export function getDiscList(){
-// 	const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
-
-// 	const data = Object.assign({}, commonParams,{
-// 		platform: 'yqq',
-// 		hostUin: 0,
-// 		sin: 0,
-// 		ein: 29,
-// 		sortId: 5,
-// 		needNewCode: 0,
-// 		categoryId: 10000000,
-// 		rnd: Math.random()
-// 	})
-
-// 	return jsonp(url, data, options)
-// }
-
 export function getDiscList() {
-  const url = '/api/getDiscList'
+  const url = '/getDiscList'
 
   const data = Object.assign({}, commonParams, {
     platform: 'yqq',
